7131dea9edbc42279411816bee7d501e3c08af59
elainees-devs <elainees.devs@gmail.com>
2025-09-30 16:24:50 +0300

    Add URL patterns for login, logout, and registration views

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+# users/urls.py
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import register
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', register, name='register'),
+]
