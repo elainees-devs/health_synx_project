29108312996dda48ff5ec84ceec1391df9396198
elainees-devs <elainees.devs@gmail.com>
2025-09-30 18:52:08 +0300

    create CRUD class-based views for Payment: list, detail, create, update, delete

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Payment
+from .forms import PaymentForm
 
-# Create your views here.
+# List all payments
+class PaymentListView(ListView):
+    model = Payment
+    template_name = "payments/payment_list.html"
+    context_object_name = "payments"
+    ordering = ['-created_at']
+
+# View payment details
+class PaymentDetailView(DetailView):
+    model = Payment
+    template_name = "payments/payment_detail.html"
+    context_object_name = "payment"
+
+# Create a new payment
+class PaymentCreateView(CreateView):
+    model = Payment
+    form_class = PaymentForm
+    template_name = "payments/payment_form.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form_title'] = "Add Payment"
+        context['submit_button'] = "Create"
+        return context
+
+# Update an existing payment
+class PaymentUpdateView(UpdateView):
+    model = Payment
+    form_class = PaymentForm
+    template_name = "payments/payment_form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form_title'] = "Edit Payment"
+        context['submit_button'] = "Update"
+        return context
+
+# Delete a payment
+class PaymentDeleteView(DeleteView):
+    model = Payment
+    template_name = "payments/payment_confirm_delete.html"
+    success_url = reverse_lazy('payment-list')
