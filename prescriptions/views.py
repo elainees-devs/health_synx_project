e3263e725e4b369ff3a8caf0b9b74b1ceed4d0e6
elainees-devs <elainees.devs@gmail.com>
2025-09-30 19:10:20 +0300

    Create CRUD class-based views for Prescription model

--- prescriptions/views.py
+++ prescriptions/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Prescription
+from .forms import PrescriptionForm
 
-# Create your views here.
+# List all prescriptions
+class PrescriptionListView(ListView):
+    model = Prescription
+    template_name = "prescriptions/prescription_list.html"
+    context_object_name = "prescriptions"
+    ordering = ['id']
+
+# View prescription details
+class PrescriptionDetailView(DetailView):
+    model = Prescription
+    template_name = "prescriptions/prescription_detail.html"
+    context_object_name = "prescription"
+
+# Create a new prescription
+class PrescriptionCreateView(CreateView):
+    model = Prescription
+    form_class = PrescriptionForm
+    template_name = "prescriptions/prescription_form.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form_title'] = "Add Prescription"
+        context['submit_button'] = "Create"
+        return context
+
+# Update an existing prescription
+class PrescriptionUpdateView(UpdateView):
+    model = Prescription
+    form_class = PrescriptionForm
+    template_name = "prescriptions/prescription_form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form_title'] = "Edit Prescription"
+        context['submit_button'] = "Update"
+        return context
+
+# Delete a prescription
+class PrescriptionDeleteView(DeleteView):
+    model = Prescription
+    template_name = "prescriptions/prescription_confirm_delete.html"
+    success_url = reverse_lazy('prescription-list')
