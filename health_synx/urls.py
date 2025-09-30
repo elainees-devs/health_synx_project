71fc10e329ed7ec52c914b89bcb9514d56991540
elainees-devs <elainees.devs@gmail.com>
2025-09-30 17:50:41 +0300

    Add doctors and appointments URL routing

--- health_synx/urls.py
+++ health_synx/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('patients.urls')),
     path('patient/', include('patients.urls')),
+    path('doctor/', include('doctors.urls')),
+    path('appointment/', include('appointments.urls')),
 ]
