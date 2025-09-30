6093e89f6569376316cba647d6c4de054f75c18f
elainees-devs <elainees.devs@gmail.com>
2025-09-30 17:34:51 +0300

    Add CRUD views for Doctor model

--- doctors/views.py
+++ doctors/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+# doctors/views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Doctor
+from .forms import DoctorForm 
 
-# Create your views here.
+# List all doctors
+def doctor_list(request):
+    doctors = Doctor.objects.all()
+    return render(request, 'doctors/doctor_list.html', {'doctors': doctors})
+
+# Doctor detail
+def doctor_detail(request, pk):
+    doctor = get_object_or_404(Doctor, pk=pk)
+    return render(request, 'doctors/doctor_detail.html', {'doctor': doctor})
+
+# Create a new doctor
+def doctor_create(request):
+    if request.method == 'POST':
+        form = DoctorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('doctor_list')
+    else:
+        form = DoctorForm()
+    return render(request, 'doctors/doctor_form.html', {'form': form})
+
+# Update a doctor
+def doctor_update(request, pk):
+    doctor = get_object_or_404(Doctor, pk=pk)
+    if request.method == 'POST':
+        form = DoctorForm(request.POST, instance=doctor)
+        if form.is_valid():
+            form.save()
+            return redirect('doctor_list')
+    else:
+        form = DoctorForm(instance=doctor)
+    return render(request, 'doctors/doctor_form.html', {'form': form})
+
+# Delete a doctor
+def doctor_delete(request, pk):
+    doctor = get_object_or_404(Doctor, pk=pk)
+    if request.method == 'POST':
+        doctor.delete()
+        return redirect('doctor_list')
+    return render(request, 'doctors/doctor_confirm_delete.html', {'doctor': doctor})
