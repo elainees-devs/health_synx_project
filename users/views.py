87823c43e33cfd26ec09ff67ad0e03a3c0a5ce4a
elainees-devs <elainees.devs@gmail.com>
2025-09-30 16:23:25 +0300

    Implement  register view to auto-login new users and redirect to homepage

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()  # automatically hashes password
+            login(request, user)  # log in user after registration
+            return redirect('homepage') 
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
