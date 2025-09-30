fcac8da169e8b10c97e8bcca2ea7b7a1a3ff530e
elainees-devs <elainees.devs@gmail.com>
2025-09-30 17:57:25 +0300

    Add CRUD views for Appointment model

--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+# appointments/views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Appointment
+from .forms import AppointmentForm 
 
-# Create your views here.
+# List all appointments
+def appointment_list(request):
+    appointments = Appointment.objects.all()
+    return render(request, 'appointments/appointment_list.html', {'appointments': appointments})
+
+# Appointment detail
+def appointment_detail(request, pk):
+    appointment = get_object_or_404(Appointment, pk=pk)
+    return render(request, 'appointments/appointment_detail.html', {'appointment': appointment})
+
+# Create a new appointment
+def appointment_create(request):
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('appointment_list')
+    else:
+        form = AppointmentForm()
+    return render(request, 'appointments/appointment_form.html', {'form': form})
+
+# Update an appointment
+def appointment_update(request, pk):
+    appointment = get_object_or_404(Appointment, pk=pk)
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST, instance=appointment)
+        if form.is_valid():
+            form.save()
+            return redirect('appointment_list')
+    else:
+        form = AppointmentForm(instance=appointment)
+    return render(request, 'appointments/appointment_form.html', {'form': form})
+
+# Delete an appointment
+def appointment_delete(request, pk):
+    appointment = get_object_or_404(Appointment, pk=pk)
+    if request.method == 'POST':
+        appointment.delete()
+        return redirect('appointment_list')
+    return render(request, 'appointments/appointment_confirm_delete.html', {'appointment': appointment})
