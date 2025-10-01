b6c14aafeaf9ce3da2fd5d5928c7eb7becd8b952
elainees-devs <elainees.devs@gmail.com>
2025-10-01 08:06:41 +0300

    Add home view and update register redirect to use home route

--- users/views.py
+++ users/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import CustomUserCreationForm
 
+def home(request):
+    return render(request, "users/login.html")
+
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()  # automatically hashes password
             login(request, user)  # log in user after registration
-            return redirect('homepage') 
+            return redirect('home') 
     else:
         form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
