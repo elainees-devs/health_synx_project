5f978dad477045cd4807f8029e9096b3e008e7c2
elainees-devs <elainees.devs@gmail.com>
2025-10-01 08:03:09 +0300

    Configure templates directory and static files handling

--- health_synx/settings.py
+++ health_synx/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'health_synx.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
