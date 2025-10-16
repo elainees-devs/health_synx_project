503c3450a107e60e917ef9fc8af1d16c7b732b44
elainees-devs <elainees.devs@gmail.com>
2025-10-16 15:38:56 +0300

    feat(departments): add DepartmentForm for creating and editing departments

---
+++ departments/forms.py
@@ -0,0 +1,8 @@
+# departments/forms.py
+from django import forms
+from .models import Department
+
+class DepartmentForm(forms.ModelForm):
+    class Meta:
+        model = Department
+        fields = ['name', 'description']
