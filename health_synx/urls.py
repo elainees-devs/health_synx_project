48e3645e7172622065f9ba9908443622c06c732e
elainees-devs <elainees.devs@gmail.com>
2025-10-17 16:36:27 +0300

    chore(urls): add login, register, and redirect-after-login routes to main URLconf

--- health_synx/urls.py
+++ health_synx/urls.py
@@ -1,22 +1,20 @@
-"""
-URL configuration for health_synx project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# health_synx/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
+
+from users import views as user_views
+
+def redirect_root(request):
+    return redirect('login') #'redirect_after_login'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('', redirect_root),  # handles the root URL
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('register/', user_views.register_user, name='register'), 
+    path('redirect-after-login/', user_views.redirect_after_login, name='redirect_after_login'),  
+    path('departments/', include('departments.urls')),
+    path('users/', include('users.urls')),
 ]
