e5b6c1ad3f886a7dbf90aa1b32f0acd3b419f0ed
elainees-devs <elainees.devs@gmail.com>
2025-10-26 23:47:00 +0300

    update gitignore file and remove unused files

--- departments/forms.py
+++
@@ -1,8 +0,0 @@
-# departments/forms.py
-from django import forms
-from .models import Department
-
-class DepartmentForm(forms.ModelForm):
-    class Meta:
-        model = Department
-        fields = ['name', 'description']
