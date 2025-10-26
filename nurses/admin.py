3a1b38f107036f1c94997dc996817ae5a0760817
elainees-devs <elainees.devs@gmail.com>
2025-10-26 23:05:35 +0300

    chore(nurses): configure NurseProfile admin with list display, search, and autocomplete

--- nurses/admin.py
+++ nurses/admin.py
@@ -1,3 +1,21 @@
+# nurse/admin.py
 from django.contrib import admin
+from .models import NurseProfile
 
-# Register your models here.
+@admin.register(NurseProfile)
+class NurseProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'get_full_name', 'get_email', 'department', 'specialization', 'license_number')
+    search_fields = ('user__username', 'user__email', 'department__name', 'specialization', 'license_number')  # required for autocomplete
+    autocomplete_fields = ('user', 'department')
+    list_filter = ('department',)
+    ordering = ('user__username',)
+
+    # Display user full name
+    def get_full_name(self, obj):
+        return obj.user.get_full_name()
+    get_full_name.short_description = 'Full Name'
+
+    # Display user email
+    def get_email(self, obj):
+        return obj.user.email
+    get_email.short_description = 'Email'
