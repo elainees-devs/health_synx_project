ea16051d1dd494ea7bb1509ab58b9f99a57ab44b
elainees-devs <elainees.devs@gmail.com>
2025-10-26 22:42:31 +0300

    feat(doctors): configure admin for DoctorProfile with search and autocomplete

--- doctors/admin.py
+++ doctors/admin.py
@@ -1,3 +1,9 @@
+# doctors/admin.py
 from django.contrib import admin
+from .models import DoctorProfile
 
-# Register your models here.
+@admin.register(DoctorProfile)
+class DoctorProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'specialization', 'license_number')
+    search_fields = ('user__username', 'user__email', 'specialization', 'license_number')
+    autocomplete_fields = ('user',)
