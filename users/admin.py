d4940efbeb1a6d8d7ccf0925b3f705f454780243
elainees-devs <elainees.devs@gmail.com>
2025-10-26 23:36:35 +0300

    feat(users): customize UserAdmin with role, department, and improved fieldsets

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,40 @@
+# users/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """Customize how the User model appears in Django Admin"""
+
+    # Fields shown in the list view
+    list_display = ('username', 'email', 'role', 'department', 'is_active', 'is_staff', 'date_joined')
+    list_filter = ('role', 'is_active', 'is_staff', 'department')
+
+    # Fields that link to the detail view
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+    # Fieldsets for viewing/editing a user in the admin panel
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'department', 'role')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    # Fields for adding a new user in the admin panel
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'username', 'email', 'first_name', 'last_name', 'role',
+                'department', 'password1', 'password2', 'is_staff', 'is_active'
+            ),
+        }),
+    )
+
+
