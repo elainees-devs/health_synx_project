0417c302bfc7ec0f76ee746efcb25435fb35f057
elainees-devs <elainees.devs@gmail.com>
2025-10-26 22:56:45 +0300

    feat: register HospitalAdminProfile model in Django admin with list and search options

--- hospital_admins/admin.py
+++ hospital_admins/admin.py
@@ -1,3 +1,11 @@
+# hospital_admins/admin.py
 from django.contrib import admin
+from .models import HospitalAdminProfile
 
-# Register your models here.
+@admin.register(HospitalAdminProfile)
+class HospitalAdminProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'department')
+    list_filter = ('department',)
+    search_fields = ('user__first_name', 'user__last_name', 'user__email')
+    readonly_fields = ()
+    ordering = ('id',)
