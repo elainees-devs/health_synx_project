5dc7d9cba8487b978f97ef1906566351b843daa5
elainees-devs <elainees.devs@gmail.com>
2025-10-26 23:38:13 +0300

    feat(users): add serializers for user registration, login, and general user data

---
+++ users/serializers.py
@@ -0,0 +1,27 @@
+# users/serializers.py
+from rest_framework import serializers
+from users.models import User
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'role', 'department']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'role', 'department']
+
+
