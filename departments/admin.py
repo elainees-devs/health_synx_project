5e788c1b7dde3f0db1f7a7de1cf246ac66cc238e
elainees-devs <elainees.devs@gmail.com>
2025-10-26 22:27:58 +0300

    refactor(departments): enhance admin configuration with search, filters, and autocomplete

--- departments/admin.py
+++ departments/admin.py
@@ -2,5 +2,34 @@
 from django.contrib import admin
 from .models import Department, DepartmentStaff
 
-admin.site.register(Department)
-admin.site.register(DepartmentStaff)
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    """Admin configuration for Department model"""
+    list_display = ('name', 'description')
+    search_fields = ('name',)  # Required for autocomplete support
+    ordering = ('name',)
+
+
+@admin.register(DepartmentStaff)
+class DepartmentStaffAdmin(admin.ModelAdmin):
+    """Admin configuration for DepartmentStaff model"""
+    list_display = ('department', 'get_staff_name', 'get_staff_role')
+    list_filter = ('department', 'staff__role')
+    autocomplete_fields = ('department', 'staff')
+    search_fields = (
+        'department__name',
+        'staff__username',
+        'staff__first_name',
+        'staff__last_name',
+        'staff__email',
+    )
+    ordering = ('department__name',)
+
+    def get_staff_name(self, obj):
+        return obj.staff.get_full_name() or obj.staff.username
+    get_staff_name.short_description = 'Staff Name'
+
+    def get_staff_role(self, obj):
+        return obj.staff.role
+    get_staff_role.short_description = 'Role'
